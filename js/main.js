e1678a7a84ff5e9e2ee90da116a4c4bed2139d75
Mostafa Ahmed <112582603+mostafasasa010@users.noreply.github.com>
2024-05-11 13:54:33 +0300

    points-sale mostafa
    points-sale mostafa

--- js/main.js
+++ js/main.js
@@ -13,25 +13,3 @@ scrollTopButton.onclick = () => {
     behavior: "smooth",
   });
 };
-
-// Start Sorting
-var searchFilter = () => {
-  let selectedColor = document.getElementById("filterByColor").value;
-  console.log(selectedColor);
-  const input = document.querySelector(".form-control");
-  const cards = document.getElementsByClassName("col");
-  console.log(cards[1]);
-  let textBox = input.value;
-  for (let i = 0; i < cards.length; i++) {
-    let title = cards[i].querySelector(".card-body");
-    if (
-      (cards[i].classList.contains(selectedColor) || selectedColor == "") &&
-      title.innerText.toLowerCase().indexOf(textBox.toLowerCase()) > -1
-    ) {
-      cards[i].classList.remove("d-none");
-    } else {
-      cards[i].classList.add("d-none");
-    }
-  }
-};
-// End Sorting

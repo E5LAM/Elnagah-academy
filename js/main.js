74fe33105a3a56dca62237fa68dc9740892b64b8
KarimGamal97 <kariimgamal1997@gmail.com>
2024-05-12 18:48:42 +0300

    karim_push-12

--- js/main.js
+++ js/main.js
@@ -44,3 +44,81 @@ var searchFilter = () => {
   }
 };
 // End Sorting
+
+var cards = document.getElementsByClassName("col");
+var currentPage = 1;
+var cardsPerPage = 6; // Number of cards displayed per page
+
+function updatePagination() {
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
+  const pagination = document.querySelector(".pagination");
+
+  // Show pagination only if there are more than 6 cards
+  if (totalPages > 1) {
+    pagination.classList.remove("d-none");
+  } else {
+    pagination.classList.add("d-none");
+    return;
+  }
+
+  // Update page info
+  const pageInfo = document.querySelector(".page-info");
+  pageInfo.textContent = `صفحة ${currentPage} من ${totalPages}`;
+
+  // Disable buttons if on first or last page
+  const prevButton = document.querySelector(".chevron_left");
+  const nextButton = document.querySelector(".chevron_right");
+  const doubleLeftButton = document.querySelector(".chevron_left_double");
+  const doubleRightButton = document.querySelector(".chevron_right_double");
+
+  prevButton.disabled = currentPage === 1;
+  nextButton.disabled = currentPage === totalPages;
+  doubleLeftButton.disabled = currentPage === 1;
+  doubleRightButton.disabled = currentPage === totalPages;
+}
+
+function displayCards() {
+  const startIndex = (currentPage - 1) * cardsPerPage;
+  const endIndex = Math.min(startIndex + cardsPerPage, cards.length);
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.add("d-none"); // Hide all cards initially
+  }
+
+  for (let i = startIndex; i < endIndex; i++) {
+    cards[i].classList.remove("d-none"); // Show cards for current page
+  }
+}
+
+function searchFilter() {
+  // ... existing search logic ...
+
+  updatePagination();
+  displayCards();
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    displayCards();
+    updatePagination();
+  }
+}
+
+function nextPage() {
+  if (currentPage < Math.ceil(cards.length / cardsPerPage)) {
+    currentPage++;
+    displayCards();
+    updatePagination();
+  }
+}
+
+function goToLastPage() {
+  currentPage = Math.ceil(cards.length / cardsPerPage);
+  displayCards();
+  updatePagination();
+}
+
+// Call updatePagination on page load to handle initial display
+updatePagination();
+displayCards();
